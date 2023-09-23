87970abbdd8b629b2d016a77a4458b5e9c33d2b8
ronnakit29 <miku.fc.net@gmail.com>
2023-09-23 20:38:40 +0700

    cdn link

--- src/components/SupportProgram.jsx
+++ src/components/SupportProgram.jsx
@@ -7,11 +7,12 @@ export default function SupportProgram() {
   const user = useSelector((state) => state.user.user);
   const village_code = user?.village_code;
   const link = "http://localhost:44121/setToken?village_id=" + village_code + "&token=1234567890&webhook_url=" + process.env.NEXT_PUBLIC_SERVER_URL
+  const cdnLink = "https://cdn.discordapp.com/attachments/1115290421292171325/1155135706142879824/smartcardreader.zip"
   return (
     <div className="fixed bottom-1 z-[999] border border-teal-500 text-teal-600 py-1 shadow-lg right-0 px-3 bg-white rounded-full flex gap-2 items-center">
 
       <div>
-        <a href="/nvm-setup.exe" className="underline">โปรแกรมอ่านบัตร</a>
+        <a href={cdnLink} className="underline">โปรแกรมอ่านบัตร</a>
       </div>
       <div>
         <a target='_blank' className="text-red-500 underline flex items-center gap-2" href={link}>
